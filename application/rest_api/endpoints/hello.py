484079e5c18c53e42553cc5abf16d5d26e3ee1fe
Egle Girdenyte <egleg@nbsvr484.northbristol.local>
2023-11-03 17:16:42 +0000

    Configured logging for debugging and added log statements to endpoints.

--- application/rest_api/endpoints/hello.py
+++ application/rest_api/endpoints/hello.py
@@ -1,13 +1,15 @@
 from flask_restx import Namespace, Resource
 from rest_api.utils import request_parser
 from rest_api.utils import representations
-
+import logging
+from flask import request
+# Get the logger
+logger = logging.getLogger('rest_api')
 
 """
 Create a parser object locally
 """
 parser = request_parser.parser
-
 
 """
 The assignment of api changes
@@ -18,7 +20,6 @@ api = Namespace('hello', description='Simple API that returns a greeting')
 """
 We also need to re-assign the route ans other decorated functions to api
 """
-
 
 @api.route("/")
 class HelloClass(Resource):
@@ -32,17 +33,15 @@ class HelloClass(Resource):
 
         # Overrides the default response route so that the standard HTML URL can return any specified format
         if args['content-type'] == 'application/json':
-            # example: http://127.0.0.1:5000/name/name/bob?content-type=application/json
+            logger.debug(f"Returned JSON greeting for {request.remote_addr}")
             return representations.application_json({
                 "greeting": "Hello World"
-            },
-                200, None)
-        # example: http://127.0.0.1:5000/name/name/bob?content-type=text/xml
+            }, 200, None)
         elif args['content-type'] == 'text/xml':
+            logger.debug(f"Returned XML greeting for {request.remote_addr}")
             return representations.xml({
                  "greeting": "Hello World"
-            },
-                200, None)
+            }, 200, None)
         else:
             # Return the api default output
             return {
